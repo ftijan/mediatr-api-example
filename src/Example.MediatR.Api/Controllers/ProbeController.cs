1247334694efd7b688a8cce3d5a082cccb5e0cdd
Filip Tijan <ftijan@gmail.com>
2021-07-02 17:57:21 +0100

    added base API solution files

---
+++ src/Example.MediatR.Api/Controllers/ProbeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example.MediatR.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProbeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
